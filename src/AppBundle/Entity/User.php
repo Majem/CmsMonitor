d3578cb785308d93094089455de9b94e8913332e
majem96@gmail.com <M@fi0z096>
2018-09-12 12:29:11 +0200

    FosUserBundle implementation

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: m.maciejewski
+ * Date: 12.09.2018
+ * Time: 12:18
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
